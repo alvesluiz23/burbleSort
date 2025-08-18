614b6631fd9d78449363aba8c20f986e01287c6c
Luiz <alvesluizeduardo23@gmail.com>
2025-08-18 17:03:49 -0300

    novo

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+
+fun main(args: Array<String>){
+    var temp: Int
+    var swapped: Boolean
+    val simpleArray = arrayOf(8,5,7,5,6,4)
+
+    for (i in 0 until simpleArray.size-1) {
+
+        for(j in 0 until simpleArray.size-i-1){
+            if(simpleArray.get(j) > simpleArray[j+1]){
+                temp = simpleArray[j+1]
+                simpleArray[j+1] = simpleArray.get(j)
+                simpleArray[j] = temp
+            }
+        }
+    }
+
+    println(simpleArray.contentToString())
+
+
+}
